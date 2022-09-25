c175c5d704e80733e92c42a7a5b39cefedad8034
suraj dubey <dubeysuraj864@gmail.com>
2022-09-25 14:34:51 +0530

    about featured done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,30 +4,46 @@ function About() {
   return (
     <div className="about">
       <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur
-        cupiditate vero ducimus fugit commodi libero nostrum inventore vel nam
-        modi?
-      </p>
+    
 
       <div className="box-container">
         <div className="box">
           {" "}
-          <span class="material-symbols-outlined">speed</span>Fast
+          <span class="icon1 icon material-symbols-outlined">speed</span>
+          
+          <span className="icon-name">Fast</span>
+          <p>Fast load times and lag free interaction, my highest priority.</p>
         </div>
         <div className="box">
-        <span class="material-symbols-outlined">
-devices
-</span> Responsive
+          <span class="icon2 icon material-symbols-outlined">
+            devices
+          </span>{" "}
+          
+          <span className="icon-name">Responsive</span>
+          <p>My layouts will work on any device, big or small</p>
         </div>
         <div className="box">
-          <span className="Intuitive"></span> Intuitive
+          <span class="icon3  icon material-symbols-outlined">
+            tips_and_updates
+          </span>{" "}
+          <span className="icon-name">Intuitive</span>
+          
+          <p>Strong preference for easy to use, intuitive UX/UI.</p>
         </div>
         <div className="box">
           {" "}
-          <span className="Dynamic"></span> Dynamic
+          <span class="icon4 icon material-symbols-outlined">
+            rocket_launch
+          </span>{" "}
+          <span className="icon-name">Dynamic</span>
+          <p>Websites don't have to be static, I love making pages come to life.</p>
         </div>
       </div>
+
+    <div className="skills">
+        
+    </div>
+
     </div>
   );
 }
