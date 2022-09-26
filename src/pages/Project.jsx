c7c5ca148ab9a57ff708c107df5905b734516183
suraj dubey <dubeysuraj864@gmail.com>
2022-09-26 15:33:37 +0530

    hello

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,13 +1,39 @@
+import Pizza from "../assets/projects/pizza.jpg";
+import Password from "../assets/projects/password.jpg";
+import Todo from "../assets/projects/todo.jpg";
+import RandomUser from "../assets/projects/randomUser.jpg";
+import Movie from "../assets/projects/movie.jpg";
+import Unit from "../assets/projects/unitMeasure.jpg";
+import "../styles/Projects.scss";
+
 function Project() {
   return (
-    <div className="project">
-      <h1>Project</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui maxime
-        voluptates fugit aliquid assumenda quidem suscipit nisi harum doloribus
-        quisquam?
-      </p>
-    </div>
+    <>
+
+    <div className="project-h1"><h1>Projects</h1> </div>
+  
+      <div className="project">
+      
+        <div className="image">
+          <img src={Pizza} alt="pizza" width={300} />
+        </div>
+        <div className="image">
+          <img src={Movie} alt="Movie" width={300} />
+        </div>
+        <div className="image">
+          <img src={Todo} alt="todo" width={300} />
+        </div>
+        <div className="image">
+          <img src={Unit} alt="unit" width={300} />
+        </div>
+        <div className="image">
+          <img src={Password} alt="password" width={300} />
+        </div>
+        <div className="image">
+          <img src={RandomUser} alt="random" width={300} />
+        </div>
+      </div>
+    </>
   );
 }
 
